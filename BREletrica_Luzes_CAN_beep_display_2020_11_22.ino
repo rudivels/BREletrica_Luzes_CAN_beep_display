757684b482a6e41a98868ebee533bbb77c989734
rudivels <rudivels@gmail.com>
2021-02-03 19:56:35 -0300

    Implementacao da rotina de controle de temperatura

--- BREletrica_Luzes_CAN_beep_display_2020_11_22.ino
+++ BREletrica_Luzes_CAN_beep_display_2020_11_22.ino
@@ -17,7 +17,10 @@
                Testando funcionalidades Display ok, beep ok 
                Falta testar entradas e saidas
   2020/12/15 - Iniciando uso de CANbus      
-               Can bus funcionando - testado com painel UP       
+               Can bus funcionando - testado com painel UP 
+  2020/01/29 - Testando luzes no conector e 
+               Falta o reset do display
+                     
 
 */
 
@@ -27,21 +30,69 @@
 #include <mcp2515.h>
 #include <mcp2515_defs.h>
 
+/* pinos de display LCD */
+
 #define  lcd_RS 7    // D7
 #define  lcd_RW 8    // D8
 #define  lcd_E  9    // D9
-#define  beep   6    //  D6 
+#define  beep   6    // D6 
+
+/* pinos de saida shift register 74HC595*/
+
+#define SR_RCLK   3  // D3 storage reg clock
+#define SR_SRCLK  4  // D4 shift reg clock
+#define SR_SER    5  // D5 serial data input
+
+
+void escreve74595(byte a)
+{
+  byte b;
+  b= a;
+  digitalWrite(SR_RCLK,0);
+  digitalWrite(SR_SRCLK,0);
+  digitalWrite(SR_SER,0);
+  if ((b && 0x01)==0x01) digitalWrite(SR_SER,1); else digitalWrite(SR_SER,0);        // 0x01
+  digitalWrite(SR_SRCLK,1);
+  digitalWrite(SR_SRCLK,0);
+  if (((b >> 1) && 0x01)==0x01) digitalWrite(SR_SER,1); else digitalWrite(SR_SER,0); // 0x02
+  digitalWrite(SR_SRCLK,1);
+  digitalWrite(SR_SRCLK,0);
+  if (((b >> 1) && 0x01)==0x01) digitalWrite(SR_SER,1); else digitalWrite(SR_SER,0); // 0x04
+  digitalWrite(SR_SRCLK,1);
+  digitalWrite(SR_SRCLK,0);
+  if (((b >> 1) && 0x01)==0x01) digitalWrite(SR_SER,1); else digitalWrite(SR_SER,0); // 0x08
+  digitalWrite(SR_SRCLK,1);
+  digitalWrite(SR_SRCLK,0);
+  if (((b >> 1) && 0x01)==0x01) digitalWrite(SR_SER,1); else digitalWrite(SR_SER,0); // 0x10  
+  digitalWrite(SR_SRCLK,1);
+  digitalWrite(SR_SRCLK,0);
+  if (((b >> 1) && 0x01)==0x01) digitalWrite(SR_SER,1); else digitalWrite(SR_SER,0); // 0x20
+  digitalWrite(SR_SRCLK,1);
+  digitalWrite(SR_SRCLK,0);
+  if (((b >> 1) && 0x01)==0x01) digitalWrite(SR_SER,1); else digitalWrite(SR_SER,0); // 0x40
+  digitalWrite(SR_SRCLK,1);
+  digitalWrite(SR_SRCLK,0);
+  if (((b >> 1) && 0x01)==0x01) digitalWrite(SR_SER,1); else digitalWrite(SR_SER,0); // 0x80
+  digitalWrite(SR_SRCLK,1);
+  digitalWrite(SR_SRCLK,0);
+  digitalWrite(SR_RCLK,1);   // serial to paralelel 
+  digitalWrite(SR_RCLK,0);    
+}
+
+
 
 #include "U8glib.h"
 #include "MsTimer2.h"
 
 char versao[10]="22nov20";
 
-U8GLIB_ST7920_128X64_4X u8g(lcd_E , lcd_RW, lcd_RS ); //13, 12, 11);  // Enable, RW, RS, RESE
+U8GLIB_ST7920_128X64_4X u8g(lcd_E , lcd_RW, lcd_RS );  // Enable, RW, RS, RESET
 // D7 = RS
 // D8 = R/W
 // D9 = E
 // Falta implementar Reset no Shiftrefister
+
+/* Variaveis globais */
 
 boolean PinFarolBaixa ;  
 boolean PinFarolAlta  ;
@@ -51,7 +102,7 @@ boolean PinLuzRe      ;
 boolean PinSetaDireita;  
 boolean PinSetaEquerda;  
 
-const int InLanterna = 53;
+/*const int InLanterna = 53;
 const int InFarolBaixo = 51;
 const int InFarolAlto = 49;
 
@@ -60,8 +111,9 @@ const int InSetaDireita = 45;
 const int InPiscaAlerta = 43; 
 const int InFreioDeMao = 41;
 const int InLuzDeFreio = 39;
-const int InLuzRe = 37;
+const int InLuzRe = 37; */
 
+/* variaveis globais que definem o estado das luzes */
 boolean BpiscaAlerta = 0;
 boolean BsetaEsquerda = 0;
 boolean BsetaDireita = 0;
@@ -183,9 +235,14 @@ void SetaEsquerdaOff()
   u8g.drawLine(0 + dX, 10 + dY, 15 + dX, 20 + dY);
 }
 
+int temp=0;
+
 void BaseDeTempo(void)
 {
  pisca_on =! pisca_on;
+ temp++;
+ if (temp==5) escreve74595(0); 
+ if (temp==10) { escreve74595(0xFF); temp=0;}
 }
 
 void beep_on(void)
@@ -199,8 +256,15 @@ void beep_off(void)
   digitalWrite(beep,LOW);
 }
 
+
 void setup() {
   pinMode(beep, OUTPUT);
+
+  pinMode(SR_RCLK, OUTPUT);
+  pinMode(SR_SRCLK, OUTPUT);   
+  pinMode(SR_SER, OUTPUT);     
+
+  
   /* pinMode(PinSetaDireita, OUTPUT);
   pinMode(PinSetaEquerda, OUTPUT);
   pinMode(PinFarolBaixa, OUTPUT);
@@ -274,7 +338,7 @@ void SequencialLuzes(void) {
 
 void Leia_Chaves(void) {
   
-   byte x; 
+  byte x; 
    /* BpiscaAlerta = digitalRead(InPiscaAlerta);
    BsetaEsquerda = digitalRead(InSetaEsquerda);
    BsetaDireita = digitalRead(InSetaDireita);
@@ -285,11 +349,13 @@ void Leia_Chaves(void) {
    BluzDeFreio = digitalRead(InLuzDeFreio);  
 */
     
-   x= BpiscaAlerta*B10000000+BsetaEsquerda*B01000000+BsetaDireita*B00100000+BfarolBaixo*B00010000+BfarolAlto*B00001000+Blanterna*B00000100+BfreioDeMao*B00000010+BluzDeFreio*B00000001;
+  x= BpiscaAlerta*B10000000+BsetaEsquerda*B01000000+BsetaDireita*B00100000+BfarolBaixo*B00010000+BfarolAlto*B00001000+Blanterna*B00000100+BfreioDeMao*B00000010+BluzDeFreio*B00000001;
     
-   Serial.print(x,BIN);
-   Serial.println(" Entradas ");
- 
+  Serial.print(x,BIN);
+  Serial.println(" Entradas ");
+
+  //escreve74595(x);
+  
   char inChar;
   if (Serial.available()) {
    inChar = (char)Serial.read();
@@ -365,11 +431,13 @@ void draw() {
 void loop() {
   // SequencialLuzes();   //
   tCAN message;
-  // Leia_Chaves();
+  Leia_Chaves();
   u8g.firstPage();
   do {
     draw();
   } while ( u8g.nextPage() );
+
+  /* rotinas de leitura CAN 
 
   if (mcp2515_check_message()) 
   {
@@ -388,4 +456,5 @@ void loop() {
                Serial.println("");
      }
    }
+  fim rotina leitura CAN */
 }
